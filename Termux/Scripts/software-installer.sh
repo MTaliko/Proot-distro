141b37d50f0dce10ce16f52322acef30f2c2f91a
13MTlko <161132690+MTaliko@users.noreply.github.com>
2024-05-23 14:20:19 -0300

    Create software-installer.sh

---
+++ Termux/Scripts/software-installer.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#Install some softwares
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "Install some components"
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+pkg update -y
+pkg install tur-repo php python python2 python3 -y
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "Install ended"
+echo "Installed tur-repo php python python2 python3 Ok!"
+echo ""
+echo ""
+echo ""
+tur-repo php python python2 python3
